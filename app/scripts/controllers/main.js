34566a6b6196fa3f00e26f9f80a88f1b15c707ad
Nicolas Fetter <nicofetter@gmail.com>
2015-11-21 11:04:52 +1300

    background video

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,66 @@
  */
 angular.module('driveBuyApp')
   .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    jQuery( document ).ready(function() {
+
+    scaleVideoContainer();
+
+    initBannerVideoSize('.video-container .poster img');
+    initBannerVideoSize('.video-container .filter');
+    initBannerVideoSize('.video-container video');
+
+    jQuery(window).on('resize', function() {
+        scaleVideoContainer();
+        scaleBannerVideoSize('.video-container .poster img');
+        scaleBannerVideoSize('.video-container .filter');
+        scaleBannerVideoSize('.video-container video');
+    });
+
+});
+
+function scaleVideoContainer() {
+
+    var height = jQuery(window).height() + 5;
+    var unitHeight = parseInt(height) + 'px';
+    jQuery('.homepage-hero-module').css('height',unitHeight);
+
+}
+
+function initBannerVideoSize(element){
+
+    jQuery(element).each(function(){
+        jQuery(this).data('height', jQuery(this).height());
+        jQuery(this).data('width', jQuery(this).width());
+    });
+
+    scaleBannerVideoSize(element);
+
+}
+
+function scaleBannerVideoSize(element){
+
+    var windowWidth = jQuery(window).width(),
+    windowHeight = jQuery(window).height() + 5,
+    videoWidth,
+    videoHeight;
+
+    console.log(windowHeight);
+
+    jQuery(element).each(function(){
+        var videoAspectRatio = jQuery(this).data('height')/jQuery(this).data('width');
+
+        jQuery(this).width(windowWidth);
+
+        if(windowWidth < 1000){
+            videoHeight = windowHeight;
+            videoWidth = videoHeight / videoAspectRatio;
+            jQuery(this).css({'margin-top' : 0, 'margin-left' : -(videoWidth - windowWidth) / 2 + 'px'});
+
+            jQuery(this).width(videoWidth).height(videoHeight);
+        }
+
+        jQuery('.homepage-hero-module .video-container video').addClass('fadeIn animated');
+
+    });
+}
   });
