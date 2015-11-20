083a699a90ecef5b12de8e946bc123983f7550af
Nicolas Fetter <nicofetter@gmail.com>
2015-11-21 08:39:38 +1300

    Initital commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name driveBuyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the driveBuyApp
+ */
+angular.module('driveBuyApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name driveBuyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the driveBuyApp
+ */
+angular.module('driveBuyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
